dac4eadcefefd6fc2ce1652a0d3683bf75baea6d
macleo <52macleo@gmail.com>
2020-06-20 17:00:38 +0800

    增加

--- app/src/main/java/scut/carson_ho/check_net/MainActivity.java
+++ app/src/main/java/scut/carson_ho/check_net/MainActivity.java
@@ -1,18 +1,30 @@
 package scut.carson_ho.check_net;
 
+import android.content.Context;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     NetWorkStateReceiver netWorkStateReceiver;
-
+    private Button btn_wifi_on,btn_wifi_off;
+    private WifiManager wifiManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取wifi管理服务
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        btn_wifi_on = (Button) findViewById(R.id.btn_wifi_on);
+        btn_wifi_off = (Button) findViewById(R.id.btn_wifi_off);
+        btn_wifi_on.setOnClickListener(this);
+        btn_wifi_off.setOnClickListener(this);
     }
 
     //在onResume()方法注册
@@ -34,5 +46,24 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(netWorkStateReceiver);
         System.out.println("注销");
         super.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_wifi_off:{
+                //获取wifi开关状态
+                //wifi打开状态则关闭
+                wifiManager.setWifiEnabled(false);
+                Toast.makeText(MainActivity.this, "wifi已关闭", Toast.LENGTH_SHORT).show();
+            }
+                break;
+            case R.id.btn_wifi_on: {
+                //关闭状态则打开
+                wifiManager.setWifiEnabled(true);
+                Toast.makeText(MainActivity.this, "wifi已打开", Toast.LENGTH_SHORT).show();
+            }
+                break;
+        }
     }
 }
